b0dfb594fc1e86b99b7e8c0b9628659d2c98ac92
Franz Gatt <franz.gatt@student.uibk.ac.at>
2022-04-07 15:44:29 +0200

    Koordinaten durch Variablen ersetzt

--- nz/main.js
+++ nz/main.js
@@ -1,11 +1,14 @@
 /* Mehrzeilige Kommentare */
 // Einzeiliger Kommentar
+let lat = -43.531111;
+let lng = 172.636667;
+let zoom = 9;
 
-var map = L.map('map').setView([-43.531111, 172.636667], 9);
+let map = L.map('map').setView([lat, lng], zoom);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-43.531111, 172.636667]).addTo(map)
+L.marker([lat, long]).addTo(map)
     .bindPopup('<h3>Christchurch</h3>')
     .openPopup();
