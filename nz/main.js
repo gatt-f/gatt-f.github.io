47b2c98204fcfc43279cb96337f1614042a59276
Franz Gatt <franz.gatt@student.uibk.ac.at>
2022-04-07 16:46:34 +0200

    Erste Etappe

--- nz/main.js
+++ nz/main.js
@@ -27,10 +27,12 @@ console.log('id="map"'); // Grund für verschiedene Anführungszeichen
 console.log(`latitude = ${lat}`); // Backticks durch Shift + Taste neben Clear erhalten // Durch $ kann man Variablen auflösen
 
 let popup = `
-<h3>${ETAPPEN[0].titel}</h3>
+<h3>${ETAPPEN[0].titel} (Etappe ${ETAPPEN[0].nr}</h3>
 <ul>
     <li>geogr. Länge: ${ETAPPEN[0].lng}</li>
     <li>geogr. Breite ${ETAPPEN[0].lat}</li>
+    <li><a href= ${ETAPPEN[0].wikipedia}">Link zur Wikipediaseite</a> </li>
+    <li><a href="${ETAPPEN[0].github}">Link zur Etappenseite</a></li>
 </ul>`
 ;
 
