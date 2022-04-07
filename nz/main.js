56f88334436c4a0806c044b02002c3442c3c3a1c
Franz Gatt <franz.gatt@student.uibk.ac.at>
2022-04-07 17:18:03 +0200

    Etappen-PopUps eingebunden

--- nz/main.js
+++ nz/main.js
@@ -46,6 +46,15 @@ L.marker([lat, lng]).addTo(map)
     .openPopup();
 
 for(let etappe of ETAPPEN) {
+    let popup = `
+<h3>${etappe.titel} (Etappe ${etappe.nr}</h3>
+<ul>
+    <li>geogr. Länge: ${etappe.lng}</li>
+    <li>geogr. Breite ${etappe.lat}</li>
+    <li><a href="${etappe.wikipedia}">Link zur Wikipediaseite</a> </li>
+    <li><a href="${etappe.github}">Link zur Etappenseite</a></li>
+</ul>`
+;
     //console.log(etappe);
-    L.marker([etappe.lat, etappe.lng]).addTo(map);
+    L.marker([etappe.lat, etappe.lng]).addTo(map).bindPopup(popup);
 }
